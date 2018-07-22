70ac07f11c16598492f23388da16e992b3382ee2
Antonio Garza <develanton@gmail.com>
2018-07-22 00:53:07 -0500

    added redirect

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, "../public/home.html"))
     });
 
-    app.get("/survey", function(req,res){
+    app.use(function(req,res){
         res.sendFile(path.join(__dirname,"../public/survey.html"))
     })
 };
